bb71fb5b113dfc2cfd67c422ebd3f398a9489fc7
Matteo Buompastore <matteo.buompastore088@gmail.com>
2024-03-25 18:05:51 +0100

    Added first validations for email and password

--- lib/features/auth/presentation/providers/providers.dart
+++ lib/features/auth/presentation/providers/providers.dart
@@ -1,17 +1,1 @@
-import 'package:teslo_shop/features/shared/infrastructure/inputs/inputs.dart';
-
-class LoginFormState {
-  final bool isPosting;
-  final bool isFormPosted;
-  final bool isValid;
-  final Email email;
-  final Password password;
-
-  LoginFormState(
-      {this.isPosting = false,
-      this.isFormPosted = false,
-      this.isValid = false,
-      this.email = const Email.pure(),
-      this.password = const Password.pure(),
-    });
-}
+export 'login_form_provider.dart';
